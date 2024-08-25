8173380bb90798f51381acd7072b8df41aa1d0c5
Maxson <sherstukmax2005@gmail.com>
2024-08-25 11:41:12 +0300

    vercel fix2

---
+++ eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+    },
+    env: {
+        browser: true,
+        node: true,
+    },
+    extends: ['eslint:recommended'],
+    rules: {},
+};
